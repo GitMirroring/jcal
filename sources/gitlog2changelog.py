d95d3bcbf4d14342c274cd3757cb3c380658bbc1
Hossein Naghdbishi <me@hossein.me>
2025-09-27 19:55:01 +0330

    Update ChangeLog

--- sources/gitlog2changelog.py
+++ sources/gitlog2changelog.py
@@ -1,124 +1,120 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # Copyright 2008 Marcus D. Hanwell <marcus@cryos.org>
 # Distributed under the terms of the GNU General Public License v2 or later
 
-import string, re, os
+import re
+import os
 
-# Execute git log with the desired command line options.
-fin = os.popen('git log --summary --stat --no-merges --date=short', 'r')
-# Create a ChangeLog file in the current directory.
-fout = open('ChangeLog', 'w')
+# Using 'with' is a standard Python 3 practice for handling files
+# and other resources, as it ensures they are properly closed.
+try:
+    git_log_process = os.popen('git log --summary --stat --no-merges --date=short', 'r')
+    changelog_file = open('ChangeLog', 'w')
+except OSError as e:
+    print(f"Error: Could not execute 'git log' or open 'ChangeLog'. Details: {e}")
+    exit(1)
 
-# Set up the loop variables in order to locate the blocks we want
-authorFound = False
-dateFound = False
-messageFound = False
-filesFound = False
-message = ""
-messageNL = False
-files = ""
-prevAuthorLine = ""
+with git_log_process as fin, changelog_file as fout:
+    # Set up loop variables to locate the desired blocks in the git log
+    authorFound = False
+    dateFound = False
+    messageFound = False
+    filesFound = False
+    message = ""
+    files = ""
+    prevAuthorLine = ""
 
-# The main part of the loop
-for line in fin:
-    # The commit line marks the start of a new commit object.
-    if string.find(line, 'commit') >= 0:
-        # Start all over again...
-        authorFound = False
-        dateFound = False
-        messageFound = False
-        messageNL = False
-        message = ""
-        filesFound = False
-        files = ""
-        continue
-    # Match the author line and extract the part we want
-    elif re.match('Author:', line) >=0:
-        authorList = re.split(': ', line, 1)
-        author = authorList[1]
-        author = author[0:len(author)-1]
-        authorFound = True
-    # Match the date line
-    elif re.match('Date:', line) >= 0:
-        dateList = re.split(':   ', line, 1)
-        date = dateList[1]
-        date = date[0:len(date)-1]
-        dateFound = True
-    # The svn-id lines are ignored
-    elif re.match('    git-svn-id:', line) >= 0:
-        continue
-    # The sign off line is ignored too
-    elif re.search('Signed-off-by', line) >= 0:
-        continue
-    # Extract the actual commit message for this commit
-    elif authorFound & dateFound & messageFound == False:
-        # Find the commit message if we can
-        if len(line) == 1:
-            if messageNL:
-                messageFound = True
+    # The main loop to process each line from the git log
+    for line in fin:
+        # A 'commit' line marks the start of a new commit object.
+        if line.startswith('commit '):
+            # Reset all flags for the new commit entry
+            authorFound = False
+            dateFound = False
+            messageFound = False
+            message = ""
+            filesFound = False
+            files = ""
+            continue
+        # Match the author line and extract the author's name
+        elif re.match('Author:', line):
+            authorList = line.split(': ', 1)
+            author = authorList[1].strip()
+            authorFound = True
+        # Match the date line and extract the date
+        elif re.match('Date:', line):
+            # Note: git log often uses multiple spaces after 'Date:'
+            dateList = re.split(r':\s+', line, 1)
+            date = dateList[1].strip()
+            dateFound = True
+        # The git-svn-id lines are ignored
+        elif 'git-svn-id:' in line:
+            continue
+        # The sign-off lines are also ignored
+        elif 'Signed-off-by' in line:
+            continue
+        # Extract the actual commit message
+        elif authorFound and dateFound and not messageFound:
+            # The message ends when a blank line is found after some message text
+            if not line.strip():
+                if message:
+                    messageFound = True
             else:
-                messageNL = True
-        elif len(line) == 4:
-            messageFound = True
-        else:
-            if len(message) == 0:
-                message = message + line.strip()
-            else:
-                message = message + " " + line.strip()
-    # If this line is hit all of the files have been stored for this commit
-    elif re.search('files changed', line) >= 0:
-        filesFound = True
-        continue
-    # Collect the files for this commit. FIXME: Still need to add +/- to files
-    elif authorFound & dateFound & messageFound:
-        fileList = re.split(' \| ', line, 2)
-        if len(fileList) > 1:
-            if len(files) > 0:
-                files = files + ", " + fileList[0].strip()
-            else:
-                files = fileList[0].strip()
-    # All of the parts of the commit have been found - write out the entry
-    if authorFound & dateFound & messageFound & filesFound:
-        # First the author line, only outputted if it is the first for that
-        # author on this day
-        authorLine = date + "  " + author
-        if len(prevAuthorLine) == 0:
-            fout.write(authorLine + "\n")
-        elif authorLine == prevAuthorLine:
-            pass
-        else:
-            fout.write("\n" + authorLine + "\n")
+                # Concatenate message parts with a space
+                message += (" " if message else "") + line.strip()
+        # The line with "files changed" marks the end of the file list summary
+        elif 'files changed' in line:
+            filesFound = True
+            continue
+        # Collect the file names for this commit
+        elif authorFound and dateFound and messageFound:
+            # File lines typically look like: ' path/to/file.py | 5 +++--'
+            fileList = line.split(' | ', 1)
+            if len(fileList) > 1:
+                filename = fileList[0].strip()
+                files += (", " if files else "") + filename
 
-        # Assemble the actual commit message line(s) and limit the line length
-        # to 80 characters.
-        commitLine = "* " + files + ": " + message
-        i = 0
-        commit = ""
-        while i < len(commitLine):
-            if len(commitLine) < i + 78:
-                commit = commit + "\n  " + commitLine[i:len(commitLine)]
-                break
-            index = commitLine.rfind(' ', i, i+78)
-            if index > i:
-                commit = commit + "\n  " + commitLine[i:index]
-                i = index+1
-            else:
-                commit = commit + "\n  " + commitLine[i:78]
-                i = i+79
+        # When all parts of a commit are found, write the entry to the ChangeLog
+        if authorFound and dateFound and messageFound and filesFound:
+            # Write the author/date line, but only if it's new for that day
+            authorLine = f"{date}  {author}"
+            if not prevAuthorLine:
+                fout.write(f"{authorLine}\n")
+            elif authorLine != prevAuthorLine:
+                fout.write(f"\n{authorLine}\n")
 
-        # Write out the commit line
-        fout.write(commit + "\n")
+            # Assemble the full commit entry and wrap it to ~78 characters
+            commitLine = f"* {files}: {message}"
+            i = 0
+            commit = ""
+            while i < len(commitLine):
+                # If the remainder fits on one line, append it and finish
+                if len(commitLine) - i <= 78:
+                    commit += f"\n  {commitLine[i:]}"
+                    break
 
-        #Now reset all the variables ready for a new commit block.
-        authorFound = False
-        dateFound = False
-        messageFound = False
-        messageNL = False
-        message = ""
-        filesFound = False
-        files = ""
-        prevAuthorLine = authorLine
+                # Find the last space within the line length for a clean word break
+                index = commitLine.rfind(' ', i, i + 78)
 
-# Close the input and output lines now that we are finished.
-fin.close()
-fout.close()
+                if index > i:
+                    commit += f"\n  {commitLine[i:index]}"
+                    i = index + 1
+                else:
+                    # No space found, so we must break the line mid-word
+                    commit += f"\n  {commitLine[i:i+78]}"
+                    i += 78
+
+            # Write the formatted commit line(s) to the file
+            fout.write(f"{commit}\n")
+
+            # Reset all variables for the next commit block
+            # This is redundant (also done at the start) but matches original structure
+            authorFound = False
+            dateFound = False
+            messageFound = False
+            message = ""
+            filesFound = False
+            files = ""
+            prevAuthorLine = authorLine
+
+# The 'with' statement automatically handles closing the files.
