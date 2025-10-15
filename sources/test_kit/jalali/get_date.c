5a24ae4d320d8bb187f4fee896cc178b285d025a
Hossein Naghdbishi <me@hossein.me>
2025-10-15 19:56:11 +0330

    tastkit(copyright): add copyright notice for tests

--- sources/test_kit/jalali/get_date.c
+++ sources/test_kit/jalali/get_date.c
@@ -1,3 +1,23 @@
+/*
+ * get_date.c - Tools for manipulating Jalali representation of Iranian calendar
+ * and necessary conversations to Gregorian calendar.
+ * Copyright (C) 2006, 2007, 2009, 2010, 2011 Ashkan Ghassemi.
+ *
+ * This file is part of libjalali.
+ *
+ * libjalali is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libjalali is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libjalali.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include "jalali.h"
