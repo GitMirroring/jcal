dd6745443dc9b0a3503bcd4cb985b6baf7a230f3
Hossein Naghdbishi <me@hossein.me>
2025-11-13 18:17:09 +0330

    Change license address from http to https

--- sources/pyjalali/helpers.py
+++ sources/pyjalali/helpers.py
@@ -12,7 +12,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Lesser General Public License for more details.
 # You should have received a copy of the GNU Lesser General Public License
-# along with pyjalali.  If not, see <http://www.gnu.org/licenses/>.
+# along with pyjalali.  If not, see <https://www.gnu.org/licenses/>.
 
 """
     pyjalali.helpers
